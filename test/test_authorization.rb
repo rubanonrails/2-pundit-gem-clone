7f1aa12c97b83cd96272e0c573f555cf4bd03429
Alexandre Ruban <alexandre@hey.com>
2023-05-28 11:59:41 +0200

    Test the PunditClone::Authorization module

---
+++ test/test_authorization.rb
@@ -0,0 +1,65 @@
+require "test_helper"
+
+class ArticlePolicy
+  attr_reader :user, :record
+
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+
+  def show?
+    user.admin? || record.published?
+  end
+
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      user.admin? ? scope.all : scope.published
+    end
+  end
+end
+
+class TestPunditCloneAuthorization < Minitest::Test
+  include PunditClone::Authorization
+
+  def setup
+    @reader = User.create! role: :reader
+    @admin  = User.create! role: :admin
+
+    @draft_article     = Article.create! title: "Draft",     status: :draft
+    @published_article = Article.create! title: "Published", status: :published
+  end
+
+  def teardown
+    User.delete_all
+    Article.delete_all
+  end
+
+  def test_policy
+    assert_kind_of ArticlePolicy,  policy(@draft_article, @reader)
+
+    assert_equal   @reader,        policy(@draft_article, @reader).user
+    assert_equal   @draft_article, policy(@draft_article, @reader).record
+  end
+
+  def test_authorize
+    assert_raises PunditClone::NotAuthorizedError do
+      authorize(@draft_article, @reader, :show?)
+    end
+
+    assert authorize(@draft_article, @admin, :show?)
+    assert authorize(@published_article, @reader, :show?)
+  end
+
+  def test_policy_scope
+    assert_equal Article.published, policy_scope(Article, @reader)
+    assert_equal Article.all,       policy_scope(Article, @admin)
+  end
+end
