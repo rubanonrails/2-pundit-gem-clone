a516ba35183f4c98426290e86fdd5210be88267f
Alexandre Ruban <alexandre@hey.com>
2023-05-28 11:12:35 +0200

    Remove boilerplate failing test

--- test/test_pundit_clone.rb
+++ test/test_pundit_clone.rb
@@ -6,8 +6,4 @@ class TestPunditClone < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::PunditClone::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
